47cb0e5b64f7ec07ca569d7543cdedfc5bedb165
david-garza <locodave@gmail.com>
2022-02-09 21:07:31 -0600

    candidate totals added

--- PyPoll.py
+++ PyPoll.py
@@ -8,7 +8,16 @@ input_file_path = os.path.join("resources","election_results.csv")
 # Define path to output file
 output_file_path = os.path.join("analysis","election_analysis.txt")
 
-# OPen electio file
+# 1. Initialize a total vote counter.
+total_votes = 0
+
+# 2. Candidate List
+candidate_options = []
+
+# 3. Dictionary to hole candiate vote totals
+candidate_votes = {}
+
+# Open election file
 with open(input_file_path) as election_data:
 
     #Read the file and load data to file_reader object (list like)
@@ -16,8 +25,31 @@ with open(input_file_path) as election_data:
 
     # Print the header row.
     headers = next(file_reader)
-    print(headers)
-    
+
+    # Loop through ever row in file_reader
+    for row in file_reader:
+        # Add to total vote count
+        total_votes += 1
+
+        # Read candidate name and save
+        candidate_name = row[2]
+
+        # Use member functions to add candidate to list
+        if candidate_name not in candidate_options:
+
+            # Add candidate to the candidate list
+            candidate_options.append(candidate_name)
+
+            # Create a candidate key in the candidate dictionary
+            candidate_votes[candidate_name] = 0
+
+        # Add a vote each time the candidate appears
+        candidate_votes[candidate_name] += 1
+
+print(total_votes)
+print(candidate_options)
+print(candidate_votes)
+
 #The data we need to retrieve
 # 1. The total number of votes cast
 # 2. A complete list of candidates who received votes
