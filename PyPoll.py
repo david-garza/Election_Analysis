34fb727ea9494f078b0c486cc7300b9b4e710125
david-garza <locodave@gmail.com>
2022-02-09 20:41:31 -0600

    Done with file read ops

--- PyPoll.py
+++ PyPoll.py
@@ -1,23 +1,23 @@
+#Add our dependencies
 import csv
 import os
 
-# The path to the file 
-file_path = os.path.join("resources","election_results.csv")
+# Define path to input file
+input_file_path = os.path.join("resources","election_results.csv")
 
-# Open the file using with
-with open(file_path) as election_data:
-    print(election_data)
+# Define path to output file
+output_file_path = os.path.join("analysis","election_analysis.txt")
 
+# OPen electio file
+with open(input_file_path) as election_data:
 
-# File to save
-path_to_output_file = os.path.join("analysis","election_analysis.txt")
+    #Read the file and load data to file_reader object (list like)
+    file_reader = csv.reader(election_data)
 
-# Create file using with
-with open(path_to_output_file,"w") as txt_file:
-    txt_file.write("Counties in the Election\n")
-    txt_file.write("-------------------------\n")
-    txt_file.write("Arapahoe\nDenver\nJefferson")
-
+    # Print the header row.
+    headers = next(file_reader)
+    print(headers)
+    
 #The data we need to retrieve
 # 1. The total number of votes cast
 # 2. A complete list of candidates who received votes
