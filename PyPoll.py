6204603873e989f60ec097adbadb30d869c15062
david-garza <locodave@gmail.com>
2022-02-11 17:20:46 -0600

    Playing with print

--- PyPoll.py
+++ PyPoll.py
@@ -17,6 +17,11 @@ candidate_options = []
 # 3. Dictionary to hole candiate vote totals
 candidate_votes = {}
 
+# 4. Winning Candidate and Trackers
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
+
 # Open election file
 with open(input_file_path) as election_data:
 
@@ -45,10 +50,41 @@ with open(input_file_path) as election_data:
 
         # Add a vote each time the candidate appears
         candidate_votes[candidate_name] += 1
+    
+    # Winning calculation
 
-print(total_votes)
-print(candidate_options)
-print(candidate_votes)
+print(f"Total votes cast: {total_votes:,}")
+
+# Find the percentage of votes
+for candidate in candidate_options:
+    votes = candidate_votes[candidate]
+    percentage = votes/total_votes*100
+    print(f"{candidate}: {percentage:.1f}% ({votes:,})")
+
+    # Determine the winning candidate
+    if (votes>winning_count) and (percentage>winning_percentage):
+        #if condition is true set winning variables to this value
+        winning_count = votes
+        winning_percentage = percentage
+        winning_candidate = candidate
+
+# Prin the winning candidate
+print(
+    f"-------------------------\n",
+    f"Winner\n",
+    f"-------------------------\n",
+    f"Candidate: {winning_candidate}\n",
+    f"Percentage: {winning_percentage:.1f}%\n",
+    f"Votes: {winning_count:,}\n")
+
+# Module code, doesn't produce indent
+winning_candidate_summary = (
+    f"-------------------------\n"
+    f"Winner: {winning_candidate}\n"
+    f"Winning Vote Count: {winning_count:,}\n"
+    f"Winning Percentage: {winning_percentage:.1f}%\n"
+    f"-------------------------\n")
+print(winning_candidate_summary)
 
 #The data we need to retrieve
 # 1. The total number of votes cast
